96b80c164264fdacd0be4e2871008e986dc70148
Karthi <100505961+theDude18@users.noreply.github.com>
2024-08-07 15:03:15 +0530

    Add files via upload

---
+++ SpringOAuthDemo/src/main/java/com/boot/springOAuth/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.boot.springOAuth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+			.oauth2Login(Customizer.withDefaults());  //creates OAuth2 loginform
+				
+		return http.build();
+	}
+}
